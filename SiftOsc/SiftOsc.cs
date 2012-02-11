b2880ea6e06a3b34e6b60763a0b534989213113c
kellydunn <defaultstring@gmail.com>
2012-02-11 12:24:01 -0800

    btichs n hose

--- SiftOsc/SiftOsc.cs
+++ SiftOsc/SiftOsc.cs
@@ -5,12 +5,12 @@ using System.IO;
 using System.Drawing;
 using Bespoke.Common;
 using Bespoke.Common.Osc;
-using Bespoke.Common.Osc;
 
 namespace SiftOsc {
   public class SiftOsc : BaseApp {
     private OscServer server;
     private OscClient client;
+    private IPAddress renoiseAddress;
     private IPEndPoint endPoint;
     private int port;
 
@@ -25,7 +25,8 @@ namespace SiftOsc {
       server.Start();
 
       client = new OscClient(IPAddress.Loopback, 7001);
-      endPoint = new IPEndPoint(IPAddress.Loopback, 9001);
+      renoiseAddress = IPAddress.Parse("10.0.2.117");
+      endPoint = new IPEndPoint(renoiseAddress, 9001);
 
       foreach(var cube in this.CubeSet) {
         cube.TiltEvent += OnTilt;
@@ -39,10 +40,6 @@ namespace SiftOsc {
       oscMessage2.Append("" + (y + 2));
       OscMessage oscMessage3 = new OscMessage(endPoint, "/renoise/song/edit/step", client);
       oscMessage3.Append("" + (z + 2));
-      //OscBundle bundle = new OscBundle(endPoint, client);
-      //bundle.Append(oscMessage);
-      //bundle.Append(oscMessage2);
-      //bundle.Send(endPoint);
       oscMessage.Send(endPoint);
       oscMessage2.Send(endPoint);
       oscMessage3.Send(endPoint);
